80e1c9e52de571759e554aa7dc2c9ddcd89277ed
elissa-murphy <elrmurph@iu.edu>
2023-04-03 22:09:09 -0400

    added config file

--- application/config.php
+++ application/config.php
@@ -1,1 +1,27 @@
 <?php
+
+
+/*
+ * Author: Louie Zhu
+ * Date: Mar 6, 2016
+ * File: config.php
+ * Description: set application settings
+ *
+ */
+
+//error reporting level: 0 to turn off all error reporting; E_ALL to report all
+error_reporting(E_ALL);
+
+//local time zone
+date_default_timezone_set('America/New_York');
+
+//base url of the application
+define("BASE_URL", "http://localhost/I211/final/i211-final");
+
+
+/*************************************************************************************
+ *                       settings for movies                                         *
+ ************************************************************************************/
+
+//define default path for media images
+define("IMG", "www/img/");
