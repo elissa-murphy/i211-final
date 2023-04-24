50a2360fdf1a1431be86cf18cbd759b5aa18c476
elissa-murphy <elrmurph@iu.edu>
2023-04-23 21:38:50 -0400

    Updated Nav

--- views/index_view.class.php
+++ views/index_view.class.php
@@ -44,9 +44,9 @@ class IndexView {
                           <a style="color: white; padding-left: 15px;" href="<?= BASE_URL ?>/user/index">
                              <div>Community</div>
                          </a>
-                         <a style="color: white; padding-left: 15px;" href="<?= BASE_URL ?>/cart/index">
-                             <div>Cart</div>
-                         </a>
+<!--                         <a style="color: white; padding-left: 15px;" href="--><?//= BASE_URL ?><!--/cart/index">-->
+<!--                             <div>Cart</div>-->
+<!--                         </a>-->
 <!--                            <a style="color: white;" href="--><?//= BASE_URL ?><!--/accessory/create">-->
 <!--                             <div>Add Accessories</div>-->
 <!--                         </a>-->
